d86a5020a925b010d5e42bcf116824248b8bf7c4
zdivelbiss <8019570+semiviral@users.noreply.github.com>
2024-10-12 13:19:40 -0500

    update gitignore

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,11 +1,5 @@
 mod email_address;
 pub use email_address::*;
 
-// mod password_digest;
-// pub use password_digest::*;
-
 mod verification_token;
 pub use verification_token::*;
-
-#[derive(Debug)]
-pub struct ConvertError;
