3f4c2e9d594d08d81691278569a94d157a080663
zdivelbiss <8019570+zdivelbiss@users.noreply.github.com>
2025-10-14 20:30:47 -0500

    add payee list

--- src/server/site/payees.rs
+++ src/server/site/payees.rs
@@ -1,44 +1,55 @@
 use askama_web::WebTemplate;
-use axum::{Router, routing};
+use axum::{Router, extract::State, response::IntoResponse, routing};
+use uuid::Uuid;
 
-use crate::state::AppState;
+use crate::{
+    server::{UserSession, internal_error},
+    state::{AppState, ledger::payee::PayeeLedger},
+};
 
 #[derive(askama::Template)]
 #[template(path = "pages/payees.html")]
 struct PayeesTemplate {}
 
-// #[derive(askama::Template)]
-// #[template(path = "partials/accounts/list.html")]
-// struct AccountsListTemplate {
-//     accounts: Box<[AccountListItem]>,
-// }
-
-pub fn router() -> Router<AppState> {
-    Router::new().route(
-        "/",
-        routing::get(|| async { WebTemplate(PayeesTemplate {}) }),
-    )
-    // .route("/list", routing::get(_list_all))
+struct PayeeListItem {
+    pub id: Uuid,
+    pub name: String,
+    pub description: Option<String>,
 }
 
-// async fn _list_all(
-//     user_session: UserSession,
-//     State(account_ledger): State<AccountLedger>,
-// ) -> impl IntoResponse {
-//     match account_ledger.read_all(user_session.id()).await {
-//         Ok(accounts) => {
-//             let accounts = accounts
-//                 .into_iter()
-//                 .map(|record| AccountListItem {
-//                     name: record.name,
-//                     kind: record.kind.friendly_name(),
-//                     description: record.description,
-//                 })
-//                 .collect::<Box<[AccountListItem]>>();
+#[derive(askama::Template)]
+#[template(path = "partials/payees/list.html")]
+struct PayeeListTemplate {
+    payee_list_items: Box<[PayeeListItem]>,
+}
 
-//             WebTemplate(AccountsListPartial { accounts }).into_response()
-//         }
+pub fn router() -> Router<AppState> {
+    Router::new()
+        .route(
+            "/",
+            routing::get(|| async { WebTemplate(PayeesTemplate {}) }),
+        )
+        .route("/list", routing::get(_list_all))
+}
 
-//         Err(error) => internal_error(error).into_response(),
-//     }
-// }
+async fn _list_all(
+    user_session: UserSession,
+    State(payee_ledger): State<PayeeLedger>,
+) -> impl IntoResponse {
+    match payee_ledger.read_all(user_session.id()).await {
+        Ok(payees) => {
+            let payee_list_items = payees
+                .into_iter()
+                .map(|record| PayeeListItem {
+                    id: record.id,
+                    name: record.name,
+                    description: record.description,
+                })
+                .collect::<Box<[PayeeListItem]>>();
+
+            WebTemplate(PayeeListTemplate { payee_list_items }).into_response()
+        }
+
+        Err(error) => internal_error(error).into_response(),
+    }
+}
